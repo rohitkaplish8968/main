5c038f8261e7c15260c28996a6c2abcfe4f32e2d
rohitkaplish8968 <rohitkaplish8968@gmail.com>
2021-10-01 16:10:59 +0530

    test pacjkage

---
+++ packages/devdojo/calculator/src/CalculatorServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace devdojo\calculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+     
+     
+    //
+    
+   
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+       include __DIR__.'/routes.php';
+    }
+}
---
+++ packages/devdojo/calculator/src/routes.php
@@ -0,0 +1,6 @@
+<?php
+
+Route::get('calculator', function(){
+	echo 'Hello from the calculator package!.<br><br>';
+	echo 'please go next step';
+});
